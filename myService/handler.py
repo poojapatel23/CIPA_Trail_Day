76a321e2ad13ebb9d4f886a4248090dc44114962
Pooja Patel <Poojapatelce@gmail.com>
2020-10-22 15:29:13 +0200

    Added Table Vie serverless yml file

--- myService/handler.py
+++ myService/handler.py
@@ -3,10 +3,12 @@ import json
 
 def hello(event, context):
 
-    data = json.loads(event['body'])
+    # data = json.loads(event['body'])
 
-    number1 = data['number1']
-    number2 = data['number2']
+    # number1 = data['number1']
+    # number2 = data['number2']
+    number1 = 20
+    number2 = 10
     sum = number1 + number2
     difference = number1 - number2
     product = number1 * number2
