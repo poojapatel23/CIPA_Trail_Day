59560faf0854203239ba407a34fb14399f1ef223
Pooja Patel <Poojapatelce@gmail.com>
2020-10-22 15:05:58 +0200

    Create a serverless project and deploy on amazon

---
+++ myService/handler.py
@@ -0,0 +1,49 @@
+import json
+
+
+def hello(event, context):
+
+    data = json.loads(event['body'])
+
+    number1 = data['number1']
+    number2 = data['number2']
+    sum = number1 + number2
+    difference = number1 - number2
+    product = number1 * number2
+    try:
+        quotient = number1 / number2
+    except ZeroDivisionError:
+        quotient = 0
+
+    # quotient = number1 / number
+
+    response_body = {
+        "Number1": number1,
+        "Number2": number2,
+        "Sum": sum,
+        "Difference": difference,
+        "Product": product,
+        "Quotient": quotient,
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(response_body)
+    }
+
+    return response
+
+    # body = {
+    #     "message": "Hello! Successfully Deploy",
+    #     "input": event
+    # }
+    #
+    # response = {
+    #     "statusCode": 200,
+    #     "body": json.dumps(body)
+    # }
+    #
+    # return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
